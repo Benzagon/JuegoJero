7411083308b892010f15e8417c5702717f7406f1
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-05-31 09:10:12 -0300

    Movimiento de Personaje y Camara

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+    public float camSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+    }
+}
---
+++ Assets/Scripts/playerMovementScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour {
+
+    Transform trans;
+
+    public GameObject playerCam;
+
+    public float moveSpeedWS, moveSpeedAD, camSpeed;
+
+	void Start () {
+        trans = GetComponent<Transform>();
+	}
+	
+	void Update () {
+        //WASD Movement
+        if (Input.GetKey(KeyCode.D))
+            trans.Translate(0, 0, -moveSpeedAD);
+        if (Input.GetKey(KeyCode.A))
+            trans.Translate(0, 0, moveSpeedAD);
+
+        if (Input.GetKey(KeyCode.W))
+            trans.Translate(moveSpeedWS, 0, 0);
+        if (Input.GetKey(KeyCode.S))
+            trans.Translate(-moveSpeedWS, 0, 0);
+
+        //Camera Movement
+        if (Input.GetAxisRaw("Mouse X") > 0)
+            trans.Rotate(0.0f, camSpeed, 0.0f);
+        if (Input.GetAxisRaw("Mouse X") < 0)
+            trans.Rotate(0.0f, -camSpeed, 0.0f);
+    }
+}
